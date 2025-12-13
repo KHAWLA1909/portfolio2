68bd98c7b096ab3ded62a2a541e1270479e28e5c
KHAWLA1909 <khawlabaida932@gmail.com>
2025-12-13 12:38:48 +0100

    all files

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const btn = document.getElementById("btn");
-btn.addEventListener("click", function() { document.body.style.backgroundColor = "pink" });
+let isBlack = false;
+
+document.getElementById("invert").addEventListener("click", function() {
+    if (isBlack) {
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+        isBlack = false;
+    } else {
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+        isBlack = true;
+    }
+});
